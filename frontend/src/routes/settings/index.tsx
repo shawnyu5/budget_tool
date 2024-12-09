b05391d1d5013e18deac857fd1493108b91546d7
shawn yu <shawn.yu5@hotmail.com>
2024-12-08 20:23:24 -0500

    feat(frontend): settings page

--- frontend/src/routes/settings/index.tsx
+++ frontend/src/routes/settings/index.tsx
@@ -1,9 +1,51 @@
+import { action } from "@solidjs/router";
+import "./index.css";
 import MonthsDropDown from "~/components/monthsDropDown";
+import { createSignal } from "solid-js";
 
-export default function () {
-  return (
-    <>
-      <MonthsDropDown />
-    </>
-  );
+export default function() {
+   const [settings, setSettings] = createSignal("");
+
+   return (
+      <>
+         <MonthsDropDown />
+         <div id="settings-form">
+            <h2>Budget allocation</h2>
+            <form action={handleSubmission} method="post">
+               <label for="month-budget">Month's budget($)</label>
+               <input
+                  type="number"
+                  id="month-budget"
+                  name="month-budget"
+                  placeholder="1000"
+                  required
+               />
+               <label for="shawn-contribution">Shawn contribution(%):</label>
+               <input
+                  type="number"
+                  id="shawn-contribution"
+                  name="shawn-contribution"
+                  placeholder="50"
+                  required
+               />
+
+               <label for="maggie-contribution">Maggie contribution(%):</label>
+               <input
+                  type="number"
+                  id="maggie-contribution"
+                  placeholder="50"
+                  name="maggie-contribution"
+                  value={settings()}
+                  required
+               />
+               <button type="submit">Submit</button>
+            </form>
+         </div>
+      </>
+   );
 }
+
+const handleSubmission = action(async (data: FormData) => {
+   console.log(`submitted: ${data}`);
+   console.log(`shawn: ${data.get("shawn-contribution")}`);
+}, "settings-form");
