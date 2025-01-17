3e7da6f822927225623c3764333568c63d559011
shawn yu <shawn.yu5@hotmail.com>
2025-01-17 11:41:59 -0500

    test(frontend): settings page dynamically updating all fields correctly

--- frontend/e2e/settings_page.spec.ts
+++ frontend/e2e/settings_page.spec.ts
@@ -1,14 +1,68 @@
-import test, { expect } from "@playwright/test";
+import test, { expect, Page } from "@playwright/test";
+
+function monthBudgetLocator(page: Page) {
+  return page.locator("#month-budget");
+}
+
+function shawnContributionPercentageLocator(page: Page) {
+  return page.locator("#shawn-contribution-percentage");
+}
+
+function maggieContributionPercentageLocator(page: Page) {
+  return page.locator("#maggie-contribution-percentage");
+}
+
+function shawnContributionAmountLocator(page: Page) {
+  return page.locator("#shawn-contribution-amount");
+}
+
+function maggieContributionAmountLocator(page: Page) {
+  return page.locator("#maggie-contribution-amount");
+}
 
 test("Settings page displays total budget and percentage split", async ({
   page,
 }) => {
   await page.goto("http://localhost:3000/settings");
-  const monthlyBudgetInput = page.locator("#month-budget");
-  await expect(monthlyBudgetInput).toHaveValue("300");
+  const monthlyBudgetField = monthBudgetLocator(page);
+  await expect(monthlyBudgetField).toHaveValue("300");
 
-  const shawnContribution = page.locator("#shawn-contribution-percentage");
-  await expect(shawnContribution).toHaveValue("60");
+  const shawnContributionPercentage = shawnContributionPercentageLocator(page);
+  await expect(shawnContributionPercentage).toHaveValue("60");
 
-  const maggieContribution = page.locator("#maggie-contribution-percentage");
-  await expect(maggieContribution).toHaveValue("40"); });
+  const maggieContributionPercentage =
+    maggieContributionPercentageLocator(page);
+  await expect(maggieContributionPercentage).toHaveValue("40");
+
+  const shawnContributionAmount = shawnContributionAmountLocator(page);
+  page.locator("#shawn-contribution-amount");
+  await expect(shawnContributionAmount).toHaveValue("180");
+
+  const maggieContributionAmount = maggieContributionAmountLocator(page);
+  await expect(maggieContributionAmount).toHaveValue("120");
+});
+
+test("Settings page fields updates dymnically", async ({ page }) => {
+  await page.goto("http://localhost:3000/settings");
+  // Set split percentage to 50/50
+  await page.fill("input#shawn-contribution-percentage", "50");
+  await page.fill("input#maggie-contribution-percentage", "50");
+
+  // Set both contribution amount to 100
+  await page.fill("input#maggie-contribution-amount", "100");
+  await page.fill("input#shawn-contribution-amount", "100");
+
+  // The total budget should be $200
+  await expect(monthBudgetLocator(page), {
+    message: "Incorrect monthly total",
+  }).toHaveValue("200");
+
+  await page.fill("input#shawn-contribution-percentage", "60");
+  // 60% of the total budget - $200 should be 120
+  await expect(shawnContributionAmountLocator(page)).toHaveValue("120");
+
+  // The total budget should remain $200
+  await expect(monthBudgetLocator(page), {
+    message: "Incorrect monthly total",
+  }).toHaveValue("200");
+});
