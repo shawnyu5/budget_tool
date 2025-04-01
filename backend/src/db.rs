8aa2db8f881be117a390330e35f30c81f7844d40
shawn yu <shawn.yu5@hotmail.com>
2025-04-01 11:53:31 -0400

    refactor(backend): move monthly_budget into its own module
    Instead of putting it with the DB mod

--- backend/src/db.rs
+++ backend/src/db.rs
@@ -2,41 +2,14 @@
 
 use anyhow::{Context, Result};
 use mongodb::{bson::doc, Client, Collection};
-use rayon::iter::{IntoParallelRefIterator, ParallelIterator};
-use serde::{Deserialize, Serialize};
 use thiserror::Error;
 use tracing::{debug, error, info};
-use utoipa::ToSchema;
 
 use crate::{
     config::Config,
     month::{Month, MonthError},
-    utils::{calculate_percentage, calculate_percentage_of},
+    monthly_budget::MonthlyBudget,
 };
-
-#[derive(Error, Debug)]
-pub enum DBError {
-    /// There are no budget information found for the current budgeting period
-    #[error("Budget not found")]
-    BudgetNotFound,
-    /// DB related errors
-    // #[error("Database related erorrs: {0}")]
-    // DB(#[from] mongodb::error::Error),
-    /// Input is an invalid year
-    #[error("Invalid year")]
-    InvalidYear,
-    /// Generic error
-    #[error("Database related error: {0}")]
-    DB(#[from] anyhow::Error),
-}
-/// Interface for database operations
-pub struct DB {
-    /// The DB client
-    // TODO: consider removing this
-    client: Client,
-    /// The current collection of the current year to operate on
-    pub collection: Collection<MonthlyBudget>,
-}
 
 impl DB {
     /// Creates a new db connection
@@ -146,173 +119,26 @@ impl DB {
         }
     }
 }
-
-/// Budget details for single month
-#[derive(Debug, Clone, PartialEq, Serialize, Deserialize, ToSchema)]
-#[serde(rename_all = "camelCase")]
-pub struct MonthlyBudget {
-    /// The month
-    pub month: Month,
-    /// Budget details
-    pub budget: Budget,
-    /// Total spending for the month. Including any over budget amount
-    #[schema(required = true)]
-    #[serde(default)]
-    pub total_spending: f64,
-    /// Amount over budget for the month. 0 means not over budget.
-    #[schema(required = true)]
-    #[serde(default)]
-    pub over_budget_amount: f64,
-    /// List of spent items
-    pub spending: Vec<SpendingItem>,
-    /// The month it was carried over from
-    /// If the setting are not carried over from a previous month, this value will be empty
-    pub carried_over_from: Option<Month>,
+#[derive(Error, Debug)]
+pub enum DBError {
+    /// There are no budget information found for the current budgeting period
+    #[error("Budget not found")]
+    BudgetNotFound,
+    /// DB related errors
+    // #[error("Database related erorrs: {0}")]
+    // DB(#[from] mongodb::error::Error),
+    /// Input is an invalid year
+    #[error("Invalid year")]
+    InvalidYear,
+    /// Generic error
+    #[error("Database related error: {0}")]
+    DB(#[from] anyhow::Error),
 }
-
-impl MonthlyBudget {
-    pub fn update_calculations(&mut self) {
-        self.calculate_total_spending();
-        self.calculate_total_budget_allocation();
-        self.calculate_over_budget_amount();
-        self.update_individual_contribution_amount();
-        self.calculate_over_budget_amount();
-    }
-
-    // /// Calculates the amount each person is contributing. This calculation assumes `self.budget.total_spending` to be up to date
-    // ///
-    // /// Updates `self.budget.shawn_contribution_amount` and `self.budget.maggie_contribution_amount`
-    // fn calculate_contribution_amount(&mut self) {
-    //     self.budget.shawn_contribution_amount = calculate_percentage_of(
-    //         self.budget.maggie_contribution_amount,
-    //         self.budget.shawn_percentage_allocation,
-    //     );
-
-    //     self.budget.maggie_contribution_amount = calculate_percentage_of(
-    //         self.budget.total_allocation,
-    //         self.budget.shawn_contribution_amount,
-    //     );
-
-    //     debug!(
-    //         "Shawn contribution amount: {}",
-    //         self.budget.shawn_contribution_amount
-    //     );
-    //     debug!(
-    //         "Maggie contribution amount: {}",
-    //         self.budget.maggie_contribution_amount
-    //     );
-    // }
-
-    /// Calculates the total spending for the month by adding up all the amounts in `self.spending`
-    ///
-    /// Updates `self.total_spending`
-    fn calculate_total_spending(&mut self) {
-        self.total_spending = self
-            .spending
-            .par_iter()
-            .map(|spend| spend.amount)
-            .sum::<f64>();
-
-        self.total_spending = (self.total_spending * 100.0).round() / 100.0;
-        debug!("Total spending: {}", self.total_spending);
-    }
-
-    /// Calculates the total allocated budget, by adding up `self.budget.maggie_contribution_amount` and `self.budget.shawn_contribution_amount`.
-    ///
-    /// Updates `self.budget.total_allocation`
-    fn calculate_total_budget_allocation(&mut self) {
-        self.budget.total_allocation =
-            self.budget.maggie_contribution_amount + self.budget.shawn_contribution_amount;
-
-        self.budget.total_allocation = (self.budget.total_allocation * 100.0).round() / 100.0;
-    }
-
-    /// Calculates the amount over budget
-    ///
-    /// Populates `self.over_budget_amount`
-    pub fn calculate_over_budget_amount(&mut self) {
-        dbg!(self.budget.total_allocation);
-        dbg!(self.total_spending);
-
-        if self.budget.total_allocation < self.total_spending {
-            self.over_budget_amount = self.total_spending - self.budget.total_allocation;
-        } else {
-            self.over_budget_amount = 0.0;
-        }
-        debug!("Over budget amount: {}", self.over_budget_amount);
-    }
-
-    /// Fills in `self.budget.*_contribution_amount` only if they are 0. Otherwise leave them be
-    pub fn update_individual_contribution_amount(&mut self) {
-        if self.budget.shawn_contribution_amount == 0.0 {
-            self.budget.shawn_contribution_amount = calculate_percentage(
-                self.budget.total_allocation,
-                self.budget.shawn_percentage_allocation,
-            );
-        }
-        if self.budget.maggie_contribution_amount == 0.0 {
-            self.budget.maggie_contribution_amount = calculate_percentage(
-                self.budget.total_allocation,
-                self.budget.maggie_percentage_allocation,
-            );
-        }
-
-        debug!(
-            "Shawn contribution amount: {}",
-            self.budget.shawn_contribution_amount
-        );
-        debug!(
-            "Maggie contribution amount: {}",
-            self.budget.maggie_contribution_amount
-        );
-    }
-}
-
-// impl MonthlyBudget {
-//     /// Check all spending records. If they dont have an ID, create one for it
-//     pub fn populate_spending_id(&mut self) {
-//         self.spending.par_iter_mut().for_each(|spending| {
-//             if spending.id.is_empty() {
-//                 spending.id = Local::now().to_string();
-//             }
-//         });
-//     }
-// }
-
-#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize, ToSchema)]
-#[serde(rename_all = "camelCase")]
-pub struct Budget {
-    /// Total allocated budget
-    #[serde(alias = "total")]
-    pub total_allocation: f64,
-    /// Shawn percentage allocation
-    #[serde(alias = "shawn_percentage_allocation")]
-    pub shawn_percentage_allocation: f64,
-    /// Shawn contribution amount. The frontend is responsible for computing this value
-    #[schema(required = true)]
-    #[serde(default)]
-    pub shawn_contribution_amount: f64,
-    /// Maggie percentage allocation
-    #[serde(alias = "maggie_percentage_allocation")]
-    pub maggie_percentage_allocation: f64,
-    /// Maggie contribution amount. The frontend is responsible for computing this value
-    #[schema(required = true)]
-    #[serde(default)]
-    pub maggie_contribution_amount: f64,
-}
-
-/// A single transaction
-#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize, ToSchema)]
-#[serde(rename_all = "camelCase")]
-pub struct SpendingItem {
-    /// A unique identifier
-    pub id: String,
-    /// The dollar amount
-    pub amount: f64,
-    /// The date
-    pub date: String,
-    /// Description of the purchase
-    pub description: String,
-    /// Additional notes
-    pub notes: Option<String>,
+/// Interface for database operations
+pub struct DB {
+    /// The DB client
+    // TODO: consider removing this
+    client: Client,
+    /// The current collection of the current year to operate on
+    pub collection: Collection<MonthlyBudget>,
 }
---
+++ backend/src/monthly_budget.rs
@@ -0,0 +1,162 @@
+use anyhow::{anyhow, Result};
+use rayon::iter::{IntoParallelRefIterator, ParallelIterator};
+use serde::{Deserialize, Serialize};
+use tracing::debug;
+use utoipa::ToSchema;
+
+use crate::{month::Month, utils::calculate_percentage};
+/// Budget details for single month
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize, ToSchema)]
+#[serde(rename_all = "camelCase")]
+pub struct MonthlyBudget {
+    /// The month
+    pub month: Month,
+    /// Budget details
+    pub budget: Budget,
+    /// Total spending for the month. Including any over budget amount
+    #[schema(required = true)]
+    #[serde(default)]
+    pub total_spending: f64,
+    /// Amount over budget for the month. 0 means not over budget.
+    #[schema(required = true)]
+    #[serde(default)]
+    pub over_budget_amount: f64,
+    /// List of spent items
+    pub spending: Vec<SpendingItem>,
+    /// The month it was carried over from
+    /// If the setting are not carried over from a previous month, this value will be empty
+    pub carried_over_from: Option<Month>,
+}
+
+impl MonthlyBudget {
+    /// Validate the budget. Returns any errors if budget amounts are incorrect
+    pub fn validate_budget(&self) -> Result<()> {
+        let total_budget =
+            self.budget.maggie_contribution_amount + self.budget.maggie_contribution_amount;
+        if self.budget.total_allocation != total_budget {
+            return Err(anyhow!("Total monthly budget is incorrect"));
+        };
+
+        if self.budget.shawn_percentage_allocation + self.budget.maggie_percentage_allocation
+            != 100.0
+        {
+            return Err(anyhow!(
+                "Incorrect percentage allocation. Both allocation does not add up to 100%"
+            ));
+        }
+
+        return Ok(());
+    }
+    #[deprecated(
+        note = "Performing the calculation should be the responsiblity of the frontend. The backend should only perform validation"
+    )]
+    pub fn update_calculations(&mut self) {
+        self.calculate_total_spending();
+        self.calculate_total_budget_allocation();
+        self.calculate_over_budget_amount();
+        self.update_individual_contribution_amount();
+        self.calculate_over_budget_amount();
+    }
+
+    /// Calculates the total spending for the month by adding up all the amounts in `self.spending`
+    ///
+    /// Updates `self.total_spending`
+    fn calculate_total_spending(&mut self) {
+        self.total_spending = self
+            .spending
+            .par_iter()
+            .map(|spend| spend.amount)
+            .sum::<f64>();
+
+        self.total_spending = (self.total_spending * 100.0).round() / 100.0;
+        debug!("Total spending: {}", self.total_spending);
+    }
+
+    /// Calculates the total allocated budget, by adding up `self.budget.maggie_contribution_amount` and `self.budget.shawn_contribution_amount`.
+    ///
+    /// Updates `self.budget.total_allocation`
+    fn calculate_total_budget_allocation(&mut self) {
+        self.budget.total_allocation =
+            self.budget.maggie_contribution_amount + self.budget.shawn_contribution_amount;
+
+        self.budget.total_allocation = (self.budget.total_allocation * 100.0).round() / 100.0;
+    }
+
+    /// Calculates the amount over budget
+    ///
+    /// Populates `self.over_budget_amount`
+    pub fn calculate_over_budget_amount(&mut self) {
+        dbg!(self.budget.total_allocation);
+        dbg!(self.total_spending);
+
+        if self.budget.total_allocation < self.total_spending {
+            self.over_budget_amount = self.total_spending - self.budget.total_allocation;
+        } else {
+            self.over_budget_amount = 0.0;
+        }
+        debug!("Over budget amount: {}", self.over_budget_amount);
+    }
+
+    /// Fills in `self.budget.*_contribution_amount` only if they are 0. Otherwise leave them be
+    pub fn update_individual_contribution_amount(&mut self) {
+        if self.budget.shawn_contribution_amount == 0.0 {
+            self.budget.shawn_contribution_amount = calculate_percentage(
+                self.budget.total_allocation,
+                self.budget.shawn_percentage_allocation,
+            );
+        }
+        if self.budget.maggie_contribution_amount == 0.0 {
+            self.budget.maggie_contribution_amount = calculate_percentage(
+                self.budget.total_allocation,
+                self.budget.maggie_percentage_allocation,
+            );
+        }
+
+        debug!(
+            "Shawn contribution amount: {}",
+            self.budget.shawn_contribution_amount
+        );
+        debug!(
+            "Maggie contribution amount: {}",
+            self.budget.maggie_contribution_amount
+        );
+    }
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize, ToSchema)]
+#[serde(rename_all = "camelCase")]
+pub struct Budget {
+    /// Total allocated budget
+    #[serde(alias = "total")]
+    pub total_allocation: f64,
+    /// Shawn percentage allocation
+    #[serde(alias = "shawn_percentage_allocation")]
+    pub shawn_percentage_allocation: f64,
+    /// Shawn contribution amount. The frontend is responsible for computing this value
+    #[schema(required = true)]
+    #[serde(default)]
+    pub shawn_contribution_amount: f64,
+    /// Maggie percentage allocation
+    #[serde(alias = "maggie_percentage_allocation")]
+    pub maggie_percentage_allocation: f64,
+    /// Maggie contribution amount. The frontend is responsible for computing this value
+    #[schema(required = true)]
+    #[serde(default)]
+    pub maggie_contribution_amount: f64,
+}
+
+/// A single transaction
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize, ToSchema)]
+#[serde(rename_all = "camelCase")]
+pub struct SpendingItem {
+    /// A unique identifier
+    pub id: String,
+    /// The dollar amount
+    pub amount: f64,
+    /// The date
+    pub date: String,
+    /// Description of the purchase
+    pub description: String,
+    /// Additional notes
+    pub notes: Option<String>,
+}
