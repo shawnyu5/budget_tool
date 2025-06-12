27ca6543ee39d52db3729bbf5b743cb1bc065ea8
shawn yu <shawn.yu5@hotmail.com>
2025-06-11 21:37:36 -0400

    feat(frontend): IOS native app

--- frontend/src/entry-server.tsx
+++ frontend/src/entry-server.tsx
@@ -8,6 +8,23 @@ export default createHandler(() => (
         <head>
           <meta charset="utf-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
+          {
+            // Have IOS treat this app as a native app, hide safari tab
+          }
+          <meta name="apple-mobile-web-app-capable" content="yes"></meta>
+          {
+             // Set app name on home page
+          }
+          <meta name="apple-mobile-web-app-title" content="Budget tool"></meta>
+
+          {
+             // Set IOS app icon
+          }
+          <link rel="apple-touch-icon" href="/logo.jpeg"></link>
+          {
+             // TODO: this startup image doesnt seem to show up for some reason
+          }
+          <link rel="apple-touch-startup-image" href="/logo.jpeg"></link>
           <link rel="icon" href="/favicon.ico" />
           {assets}
         </head>
