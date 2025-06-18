a343023b77f347c61c60db3a841feaed2109790e
shawn yu <shawn.yu5@hotmail.com>
2025-06-17 20:54:33 -0400

    fix(frontend): edit spending item page is not loading properly

--- frontend/src/routes/spending-item/edit/[year]/[month]/[id].tsx
+++ frontend/src/routes/spending-item/edit/[year]/[month]/[id].tsx
@@ -1,13 +1,6 @@
-import { action, useNavigate, useParams } from "@solidjs/router";
+import { useNavigate, useParams } from "@solidjs/router";
 import axios, { isAxiosError } from "axios";
-import {
-  Accessor,
-  createSignal,
-  ErrorBoundary,
-  onMount,
-  Setter,
-  Show,
-} from "solid-js";
+import { Accessor, createSignal, onMount, Setter, Show } from "solid-js";
 import ErrorComponent from "~/components/errorComponent";
 import { SpendingItemForm } from "~/components/spendingItemForm";
 import { loadLocalConfig } from "~/config";
@@ -18,15 +11,18 @@ export default function () {
   const params = useParams();
   const year = params.year;
   const month = params.month;
+  const spendingItemID = params.id;
   const navigate = useNavigate();
 
-  const [spendingItem, setSpendingItem] = createSignal<SpendingItem | null>(null);
+  const [spendingItem, setSpendingItem] = createSignal<SpendingItem | null>(
+    null,
+  );
   const [errorMessage, setErrorMessage] = createSignal<string | null>(null);
 
   onMount(async () => {
     try {
       const res = await axios.get<SpendingItem>(
-        `${loadLocalConfig().backendUrl}/spending-item/${year}/${month}/${spendingItem()?.id}`,
+        `${loadLocalConfig().backendUrl}/spending-item/${year}/${month}/${spendingItemID}`,
         {
           headers: {
             Authorization: `Bearer ${localStorage.getItem("token")}`,
