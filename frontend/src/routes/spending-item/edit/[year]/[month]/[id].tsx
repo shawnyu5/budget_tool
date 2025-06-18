a50b5b46aa57732b0018c29f48ae054d96541f0e
shawn yu <shawn.yu5@hotmail.com>
2025-06-17 20:34:52 -0400

    fix(frontend): better error handling on edit spending-item page

--- frontend/src/routes/spending-item/edit/[year]/[month]/[id].tsx
+++ frontend/src/routes/spending-item/edit/[year]/[month]/[id].tsx
@@ -1,11 +1,12 @@
 import { action, useNavigate, useParams } from "@solidjs/router";
-import axios from "axios";
+import axios, { isAxiosError } from "axios";
 import {
   Accessor,
   createSignal,
   ErrorBoundary,
   onMount,
   Setter,
+  Show,
 } from "solid-js";
 import ErrorComponent from "~/components/errorComponent";
 import { SpendingItemForm } from "~/components/spendingItemForm";
@@ -48,7 +49,9 @@ export default function () {
       });
     } catch (e) {
       log.error("Failed to get spending information: ", e);
-      setErrorMessage(`Failed to get spending information: ${e}`);
+      if (isAxiosError(e)) {
+        setErrorMessage(`Failed to get spending information: ${e.message}`);
+      }
     }
   });
 
@@ -94,95 +97,12 @@ export default function () {
     navigate(`/?year=${year}&month=${month}`, { replace: true });
   };
 
-  return <SpendingItemForm spendingItem={spendingItem} onSubmit={onSubmit} />;
-  // return (
-  //   <form
-  //     id="spending-item"
-  //     method="post"
-  //     action={action(async () => {
-  //       if (errorMessage()) {
-  //         log.info(
-  //           "There is an error message on screen. Not submitting form...",
-  //         );
-  //         return;
-  //       }
-  //       log.info("Submitting form");
-
-  //       const spendingItem: SpendingItem = {
-  //         id: id(),
-  //         amount: amount(),
-  //         date: date(),
-  //         description: description(),
-  //         notes: notes(),
-  //       };
-  //       // TODO: error handling
-  //       const response = await axios.post(
-  //         `${loadConfig().backendUrl}/spending-item/${year}/${month}/${id()}`,
-  //         spendingItem,
-  //         {
-  //           headers: {
-  //             Authorization: `Bearer ${localStorage.getItem("token")}`,
-  //           },
-  //         },
-  //       );
-
-  //       navigate(`/?year=${year}&month=${month}`, { replace: true });
-  //     })}
-  //   >
-  //     <ErrorComponent message={errorMessage()} />
-
-  //     <label>Amount ($)</label>
-  //     <input
-  //       name="amount"
-  //       type="number"
-  //       step="0.01"
-  //       required
-  //       value={amount()}
-  //       onInput={(e: InputEvent) => {
-  //         const input = (e.target as HTMLInputElement).value;
-  //         setAmount(parseFloat(input));
-  //       }}
-  //     />
-
-  //     <label>Date</label>
-  //     <input
-  //       name="date"
-  //       type="text"
-  //       required
-  //       value={date()}
-  //       onInput={(e: InputEvent) => {
-  //         const input = (e.target as HTMLInputElement).value;
-  //         setDate(input);
-  //       }}
-  //     />
-
-  //     <label>Description</label>
-  //     <input
-  //       name="description"
-  //       type="text"
-  //       required
-  //       value={description()}
-  //       onInput={(e: InputEvent) => {
-  //         const input = (e.target as HTMLInputElement).value;
-  //         setDescription(input);
-  //       }}
-  //     />
-
-  //     <label>Notes</label>
-  //     <textarea
-  //       name="notes"
-  //       style="height: 100px"
-  //       // type="text"
-  //       value={notes()}
-  //       onInput={(e: InputEvent) => {
-  //         const input = (e.target as HTMLInputElement).value;
-  //         setNotes(input);
-  //       }}
-  //     />
-
-  //     <button class="button success" type="submit">
-  //       Save
-  //     </button>
-  //   </form>
-  // );
+  return (
+    <div id="spending-item-form">
+      <Show when={errorMessage()}>
+        <ErrorComponent message={errorMessage()} />
+      </Show>
+      <SpendingItemForm spendingItem={spendingItem} onSubmit={onSubmit} />;
+    </div>
+  );
 }
