7df60ac96ec0aa4d7634ec46483169a202ad6487
shawn yu <shawn.yu5@hotmail.com>
2025-10-26 11:54:30 -0400

    feat: initial iOS app

---
+++ iOS/BudgetApp/BudgetApp/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  BudgetApp
+//
+//  Created by Shawn Yu on 2025-10-26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ iOS/BudgetApp/BudgetAppTests/BudgetAppTests.swift
@@ -0,0 +1,16 @@
+//
+//  BudgetAppTests.swift
+//  BudgetAppTests
+//
+//  Created by Shawn Yu on 2025-10-26.
+//
+
+import Testing
+
+struct BudgetAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
