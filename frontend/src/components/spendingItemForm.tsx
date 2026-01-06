8586e7d3747a5f6c58dc99d7f1dcf3147b41c129
shawn yu <shawn.yu5@hotmail.com>
2026-01-06 18:55:28 -0500

    feat(frontend): show individual split on per spending item

--- frontend/src/components/spendingItemForm.tsx
+++ frontend/src/components/spendingItemForm.tsx
@@ -1,7 +1,17 @@
-import { action } from "@solidjs/router";
-import { Accessor, createEffect, createSignal, Setter, Show } from "solid-js";
+import { action, Navigate, useNavigate, useParams } from "@solidjs/router";
+import {
+  Accessor,
+  createEffect,
+  createResource,
+  createSignal,
+  Setter,
+  Show,
+} from "solid-js";
 import { SpendingItem } from "~/server";
 import ErrorComponent from "./errorComponent";
+import { calculatePercentage, calculatePercentageOf } from "~/utils";
+import { handleGraphQLError, NewGraphQLSDK } from "~/graphql";
+import { BudgetConfig, Month } from "~/generated/graphql";
 
 /**
  * A form that displays a spending item
@@ -14,11 +24,54 @@ export function SpendingItemForm(props: {
     setErrorMessage: Setter<string | null>,
   ) => Promise<void>;
 }) {
+  const param = useParams();
+  const year = param.year;
+  const month = param.month;
+
+  const navigate = useNavigate();
+
   const [amount, setAmount] = createSignal(0);
   const [date, setDate] = createSignal("");
   const [description, setDescription] = createSignal("");
   const [notes, setNotes] = createSignal("");
   const [errorMessage, setErrorMessage] = createSignal<string | null>(null);
+  const [budgetConfig] = createResource(async () => {
+    const graphql = NewGraphQLSDK();
+    let response = await graphql.GetMonthlyBudgetConfig({
+      year: parseInt(year),
+      // @ts-ignore
+      month: month,
+    });
+
+    if (response.monthlyBudgetConfig.__typename == "GraphQLErrorObject") {
+      const err = handleGraphQLError(response.monthlyBudgetConfig, navigate);
+      if (err) {
+        throw new Error(err);
+      }
+    }
+    return response.monthlyBudgetConfig as BudgetConfig;
+  });
+  const shawnSplit = () => {
+    if (Number.isNaN(amount())) {
+      return 0;
+    }
+
+    return calculatePercentage(
+      amount(),
+      budgetConfig()?.shawnPercentageAllocation ?? 0,
+    );
+  };
+
+  const maggieSplit = () => {
+    if (Number.isNaN(amount())) {
+      return 0;
+    }
+
+    return calculatePercentage(
+      amount(),
+      budgetConfig()?.maggiePercentageAllocation ?? 0,
+    );
+  };
 
   createEffect(() => {
     setAmount(props.spendingItem()?.amount ?? 0);
@@ -100,6 +153,9 @@ export function SpendingItemForm(props: {
         <button class="button success" type="submit">
           Save
         </button>
+
+        <p>Shawn - ${shawnSplit()}</p>
+        <p>Maggie - ${maggieSplit()}</p>
       </Show>
     </form>
   );
