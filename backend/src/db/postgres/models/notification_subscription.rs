c9b78e4c6e06db2b74655519783b481fa3235670
shawn yu <shawn.yu5@hotmail.com>
2026-04-04 17:48:47 -0400

    feat: recreate notification_subscription table
    Remove the user of JSON. Dealing with JSON in sqlx sucks... I WANT MY TYPES

--- backend/src/db/postgres/models/notification_subscription.rs
+++ backend/src/db/postgres/models/notification_subscription.rs
@@ -1,15 +1,13 @@
-use chrono::NaiveDate;
-use sqlx::{prelude::FromRow, types::Json};
+use sqlx::prelude::FromRow;
 use uuid::Uuid;
-
-use crate::routes::notification::NotificationKeys;
 
 #[derive(Debug, FromRow, Clone)]
 pub struct NotificationSubscriptionRow {
     pub id: Uuid,
     pub user_id: Uuid,
     /// The endpoint to send notification to
-    pub endpoint: Option<String>,
-    pub expiration_time: Option<i64>,
-    pub keys: Option<Json<NotificationKeys>>,
+    pub endpoint: String,
+    pub expiration_time: Option<String>,
+    pub p256dh: String,
+    pub auth: String,
 }
