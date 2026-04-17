b3df7ddb313377c6b12cdbaa77ff290208844b62
shawn yu <shawn.yu5@hotmail.com>
2026-04-16 22:53:57 -0400

    feat(frontend): replace nav bar with bootstrap

--- frontend/src/components/navBar.tsx
+++ frontend/src/components/navBar.tsx
@@ -1,16 +1,17 @@
 import { createEffect, For, Show } from "solid-js";
 import "./monthsDropDown.css";
-import { useLocation, useNavigate, useSearchParams } from "@solidjs/router";
+import { A, useLocation, useNavigate, useSearchParams } from "@solidjs/router";
 import { monthNumberToName, setLocalAuthToken } from "~/utils";
 import log from "~/logger";
 import { exportCSV } from "~/server";
+import { Container, Form, Nav, Navbar, NavDropdown } from "solid-bootstrap";
 
 /**
- * A dropdown menu that contains the selected month.
+ * Custom Nav bar at the top of the page
  *
  * Puts the selected month in the query param, in the `month` param
  */
-export default function NavBar() {
+export default function CustomNavBar() {
   const [searchParam, setSearchParam] = useSearchParams();
   const date = new Date();
   const navigate = useNavigate();
@@ -46,6 +47,88 @@ export default function NavBar() {
   // Support the current and previous year
   const years = [date.getFullYear(), date.getFullYear() - 1];
 
+  return (
+    <Navbar bg="light" expand="lg">
+      <Container fluid>
+        {
+          // TODO: consider adding logo here
+        }
+        <Navbar.Brand as={A} href="/">
+          Big Cry baby Budget
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav>
+            <Form.Select
+              size="sm"
+              name="year"
+              value={searchParam.year}
+              onChange={(e) => setSearchParam({ year: e.currentTarget.value })}
+            >
+              <For each={years}>
+                {(year) => (
+                  <option
+                    value={year}
+                    selected={searchParam.year == year.toString()}
+                  >
+                    {year}
+                  </option>
+                )}
+              </For>
+            </Form.Select>
+            <Form.Select
+              size="sm"
+              name="month"
+              value={searchParam.month}
+              onChange={(e) => setSearchParam({ year: e.currentTarget.value })}
+            >
+              <For each={months}>
+                {(month) => (
+                  <option value={month} selected={searchParam.month == month}>
+                    {month}
+                  </option>
+                )}
+              </For>
+            </Form.Select>
+            <Show when={location.pathname == "/"}>
+              <Nav.Link
+                as={A}
+                href={`/settings?month=${searchParam.month}&year=${searchParam.year}`}
+              >
+                Settings
+              </Nav.Link>
+            </Show>
+            <Show when={location.pathname == "/settings"}>
+              <Nav.Link
+                as={A}
+                href={`/?month=${searchParam.month}&year=${searchParam.year}`}
+              >
+                Home
+              </Nav.Link>
+            </Show>
+            <Nav.Link
+              onClick={async () => {
+                const response = await exportCSV(
+                  searchParam.year as string,
+                  searchParam.month as string,
+                );
+                const blob = new Blob([response], { type: "text/plain" });
+                log.info(`blob: ${blob}`);
+
+                const link = document.createElement("a");
+                link.href = URL.createObjectURL(blob);
+                link.download = `dating_budget_${searchParam.year}_${searchParam.month}.csv`; // Set the desired file name
+                link.click();
+                URL.revokeObjectURL(link.href);
+              }}
+            >
+              Export
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
   return (
     <div id="nav-bar" class="top-bar">
       <div class="top-bar-left">
