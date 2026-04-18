f7651b53ba0affdd9a4ed10e56b4e5e6fd5b74a9
shawn yu <shawn.yu5@hotmail.com>
2026-04-18 18:24:57 -0400

    feat(backend): send notifications using postgres data

--- backend/src/cron.rs
+++ backend/src/cron.rs
@@ -2,10 +2,11 @@ use std::collections::HashMap;
 
 use crate::{
     config::Config,
-    db::{MongoDB, users::USER_TABLE_NAME},
+    db::{MongoDB, postgres::PostgresDB, users::USER_TABLE_NAME},
     routes::notification::NotificationBody,
 };
 use anyhow::{Context, Result};
+use chrono_tz::America::Toronto;
 use tokio_cron_scheduler::{Job, JobScheduler, job::job_data::Uuid};
 use tracing::{debug, info};
 use web_push::{
@@ -13,44 +14,46 @@ use web_push::{
     WebPushMessageBuilder,
 };
 
-/// Initialize all user crons
+/// Initialize user crons for sending end of month notifications
 pub async fn init_all_user_crons(scheduler: &JobScheduler) -> Result<HashMap<String, Uuid>> {
-    let db = MongoDB::new(USER_TABLE_NAME).await?;
-    let users = db.get_all_users().await.context("Failed to get users")?;
+    let db = PostgresDB::new().await;
+    let mut tx = db
+        .transaction()
+        .await
+        .context("Failed to create DB transaction")?;
+    let users = db
+        .get_core_users(&mut tx)
+        .await
+        .context("Failed to get core users from DB")?;
+    // let users = db.get_all_users().await.context("Failed to get users")?;
     let mut cron_job_ids = HashMap::new();
 
-    for user in users.clone() {
+    for user in users {
         info!(
             "Initializing end of month Cron jobs for user {}",
             user.username
         );
+        let notification = db
+            .get_user_notification_subscription(&mut tx, &user.username)
+            .await
+            .context("Failed to get user notification subscription")?;
 
-        let username = user.username.clone();
         let cron_id = scheduler
             .add(Job::new_async_tz(
                 "0 0 21 L * *",
-                chrono_tz::America::New_York,
+                Toronto,
                 move |_uuid, _l| {
                     info!("Sending end of month notification");
-                    let endpoint = user.notification_subscription.endpoint.clone();
-                    let p256dh = user.notification_subscription.keys.p256dh.clone();
-                    let auth = user.notification_subscription.keys.auth.clone();
-                    dbg!(&endpoint, &p256dh, &auth);
+                    let endpoint = notification.endpoint.clone();
+                    let p256dh = notification.keys.p256dh.clone();
+                    let auth = notification.keys.auth.clone();
 
-                    let username_clone = username.clone();
+                    // Need to clone here cuz we can't move user obj into the closure. We need to refer to it down below
                     Box::pin(async move {
-                        let db = MongoDB::new(USER_TABLE_NAME)
-                            .await
-                            .expect("Failed to connect to user DB");
-                        let user = db
-                            .get_user(&username_clone)
-                            .await
-                            .expect("Failed to get user");
-
                         send_notification(
-                            &user.notification_subscription.endpoint,
-                            &user.notification_subscription.keys.p256dh,
-                            &user.notification_subscription.keys.auth,
+                            &endpoint,
+                            &p256dh,
+                            &auth,
                             NotificationBody {
                                 title: "Reminder: end of the month!".to_string(),
                                 body: "Time to settle the budget 😊".to_string(),
@@ -67,6 +70,7 @@ pub async fn init_all_user_crons(scheduler: &JobScheduler) -> Result<HashMap<Str
     Ok(cron_job_ids)
 }
 
+/// Sends a web push notification
 async fn send_notification(
     endpoint: &str,
     p256dh: &str,
